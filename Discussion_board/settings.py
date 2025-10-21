3b586279ec019948c35ad5962d10ea1ebadc193d
Amera <ameraelbassal552005@gmail.com>
2025-10-21 17:04:45 +0300

    add first migrations to boards app

--- Discussion_board/settings.py
+++ Discussion_board/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Discussion_board.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Dissussion_Board',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
