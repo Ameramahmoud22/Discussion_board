cbd4c6b415f8b58ce73234faed22d09be1e7fa6b
Amera <ameraelbassal552005@gmail.com>
2025-10-23 15:23:28 +0300

    modify topics.html file to make Boards header is link to return back to Boards page and change  the name "index" to home i url home in url file

--- boards/url.py
+++ boards/url.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
 
-    path('', views.home, name='index'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('boards/<int:board_id>/', views.board_topics, name='board_topics')
 ]
