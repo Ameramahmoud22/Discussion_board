b1dd7a0067fefe1a15e8b349939dce16dfd90483
Amera <ameraelbassal552005@gmail.com>
2025-10-23 15:05:21 +0300

    .

--- boards/url.py
+++ boards/url.py
@@ -3,6 +3,6 @@ from . import views
 urlpatterns = [
 
     path('', views.home, name='index'),
-    path('about/' .views.about, name='about'),
+    path('about/', views.about, name='about'),
     path('boards/<int:board_id>/', views.board_topics, name='board_topics')
 ]
