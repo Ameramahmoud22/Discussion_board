a0bbab222f8b99c54dd7b87af0474cd8dadedb7e
Amera <ameraelbassal552005@gmail.com>
2025-10-24 15:49:48 +0300

    Add the redirection logic in new_topic function

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Board, Topic, Post
+from django.shortcuts import render, get_object_or_404 , redirect
+from django.http import HttpResponse, Http404 
+from .models import Board
+from .models import User
+from .models import Topic , Post
+
 # Create your views here.
 
 
@@ -25,5 +28,24 @@ def about(request):
 
 def new_topic(request, board_id):
     board = get_object_or_404(Board, pk=board_id)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']  
+        user = User.objects.first()  # temporary, until we add authentication
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            created_by=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+
+        return redirect('board_topics', board_id=board.pk)
 
     return render(request, 'new_topic.html', {'board': board})
