074cc3d8bb8bd7da32b4ca712f77ebdb59e9f103
Amera <ameraelbassal552005@gmail.com>
2025-10-26 14:37:08 +0300

    include django auth.urls in main url file

--- Discussion_board/urls.py
+++ Discussion_board/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('boards.url')),
-    path('', include('accounts.url'))
+    path('', include('accounts.url')),
+    path('accounts/', include('django.contrib.auth.urls'))
 
 ]
