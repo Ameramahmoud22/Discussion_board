104a0f939cfd447fbf6d84b797401fafbd236aa7
Amera <ameraelbassal552005@gmail.com>
2025-10-26 20:38:35 +0300

    add change_password path in url file

--- accounts/url.py
+++ accounts/url.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('settings/change_password/', auth_views.PasswordChangeView.as_view(
+        template_name='change_password.html'), name='change_password'),
 
 ]
