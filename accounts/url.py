c04b843a598c02b1456d0796339b1ee5d2bc93aa
Amera <ameraelbassal552005@gmail.com>
2025-10-26 14:51:39 +0300

    name it with login

--- accounts/url.py
+++ accounts/url.py
@@ -5,6 +5,6 @@ urlpatterns = [
 
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html')),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 
 ]
