4872f615416e0f19c97e5964ea93460fbca1b833
Amera <ameraelbassal552005@gmail.com>
2025-10-26 14:45:11 +0300

    add save function to save email as it's not contained in built in django auth

--- Discussion_board/settings.py
+++ Discussion_board/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'widget_tweaks' ,
+    'widget_tweaks',
     'boards',
     'accounts'
 ]
@@ -124,6 +124,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGOUT_REDIRECT_URL = 'home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
