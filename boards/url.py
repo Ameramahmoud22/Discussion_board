409bbb9cab3bc5255f0aed9e3d906ada007f26a0
Amera <ameraelbassal552005@gmail.com>
2025-10-27 15:02:41 +0300

    import PostForm in boards view

--- boards/url.py
+++ boards/url.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('boards/<int:board_id>/new/', views.new_topic, name='new_topic'),
     path('boards/<int:board_id>/topics/<int:topic_id>/', views.topic_posts, name='topic_posts'),
     path('boards/<int:board_id>/topics/<int:topic_id>/reply/', views.reply_topic, name='reply_topic'),
+    
 
 
 ]
