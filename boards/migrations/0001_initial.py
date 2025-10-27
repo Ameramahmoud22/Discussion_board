ad103f7d11fb32e26b90def5fa8a585169902801
Amera <ameraelbassal552005@gmail.com>
2025-10-27 23:25:27 +0300

    this migration is specially for fix the error of created_dt  to create_dt

--- boards/migrations/0001_initial.py
+++ boards/migrations/0001_initial.py
@@ -17,7 +17,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Board',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=30, unique=True)),
                 ('description', models.CharField(max_length=180)),
             ],
@@ -25,21 +26,27 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Topic',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('subject', models.CharField(max_length=250)),
-                ('created_dt', models.DateTimeField(auto_now_add=True)),
-                ('board', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='topics', to='boards.board')),
-                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='topics', to=settings.AUTH_USER_MODEL)),
+                ('create_dt', models.DateTimeField(auto_now_add=True)),
+                ('board', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                 related_name='topics', to='boards.board')),
+                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                 related_name='topics', to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('message', models.TextField(max_length=4000)),
                 ('create_dt', models.DateTimeField(auto_now_add=True)),
-                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='posts', to=settings.AUTH_USER_MODEL)),
-                ('topic', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='posts', to='boards.topic')),
+                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                 related_name='posts', to=settings.AUTH_USER_MODEL)),
+                ('topic', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                 related_name='posts', to='boards.topic')),
             ],
         ),
     ]
